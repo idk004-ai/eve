5a8f890dd22e6158a10b213568a2b543bf8ddc0b
idk004-ai <KhoiLNMHE180029@fpt.edu.vn>
2025-05-19 15:05:10 +0700

    feat: code base

---
+++ eve/src/main/java/com/group3/eve/EveApplication.java
@@ -0,0 +1,13 @@
+package com.group3.eve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveApplication.class, args);
+	}
+
+}
